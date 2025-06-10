85caa1cca319190ae1e7a8a2110d67da58b0bd95
Michael <michaelpetersen8989+github@gmail.com>
2025-06-10 13:26:29 -0500

    Add configuration files and rename main file

--- Home.py
+++ Home.py
@@ -4,6 +4,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -12,8 +14,11 @@ def add_todo():
 
 st.title("My To-Do App")
 st.subheader("This is my to-do app.")
-st.write("This app will help to increase productivity.")
+st.write("This app will help to increase <b>productivity</b>.", unsafe_allow_html=True)
 
+
+st.text_input(label="", placeholder="Add a new to-do...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -23,5 +28,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()   #st.experimental_rerun was phased out in like 2021, now it's st.rerun()
 
-st.text_input(label="", placeholder="Add a new to-do...",
-              on_change=add_todo, key='new_todo')
